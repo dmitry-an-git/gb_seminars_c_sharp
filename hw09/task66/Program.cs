dcacc4976b213b6ba58062dc129762cdf15e6b08
Dmitry An <dmitry.andreyev@gmail.com>
2022-12-26 17:22:56 +0300

    homework09 task66

---
+++ hw09/task66/Program.cs
@@ -0,0 +1,21 @@
+﻿// task66
+// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+int RangeSum(int start, int finish)
+{
+    if (start - finish == 0)
+        return start;
+    else 
+
+        return finish+RangeSum(start, finish-1);
+}
+
+Console.Clear();
+Console.Write("Задайте промежуток: ");
+int[] range = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+
+Console.WriteLine(RangeSum(range[0],range[1]));
