cc81538d3594cade3ede6114df109f891c7ae398
Dmitry An <dmitry.andreyev@gmail.com>
2022-12-26 17:23:28 +0300

    seminar09

---
+++ s09/tasks/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int rec(int a, int b)
+{
+    if (b==0) return 1;
+    else return a*rec(a,b-1);
+}
+
+Console.Write("Задайте число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Задайте число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(rec(m,n));
